057addf1d6a7ac72ca2f15a2f5bcd2c3c154d4bb
Alejandro Baltra <abaltra@gmail.com>
2017-09-26 13:09:24 -0300

    Update for patch 7.19
     * Stopped using soon to be deprecated /recent endpoint
     * Updated queueIds to comply to patch 7.19 (https://discussion.developer.riotgames.com/content/kbentry/3482/multiple-queueids-are-being-updated-with-patch-719.html)

--- riot.py
+++ riot.py
@@ -11,17 +11,17 @@ from errors import RiotRateError, RiotInvalidKeyError, RiotDataNotFoundError, Ri
 
 #All queues
 QUEUE_ID_TO_VALUE_MAP = {
-	0: "CUSTOM", 8: "NORMAL_3x3", 2: "NORMAL_5x5_BLIND", 14: "NORMAL_5x5_DRAFT",
+	0: "CUSTOM", 460: "NORMAL_3x3", 430: "NORMAL_5x5_BLIND", 14: "NORMAL_5x5_DRAFT",
 	4: "RANKED_SOLO_5x5", 6: "RANKED_PREMADE_5x5", 42: "RANKED_TEAM_5x5",
 	410: "TEAM_BUILDER_DRAFT_RANKED_5x5", 420: "TEAM_BUILDER_RANKED_SOLO",
-	440: "RANKED_FLEX_SR", 9: "RANKED_FLEX_TT", 41: "RANKED_TEAM_3x3",
-	16: "ODIN_5x5_BLIND", 17: "ODIN_5x5_DRAFT", 25: "BOT_ODIN_5x5", 31: "BOT_5x5_INTRO",
-	32: "BOT_5x5_BEGINNER", 33: "BOT_5x5_INTERMEDIATE", 52: "BOT_TT_3x3",
-	61: "GROUP_FINDER_5x5", 65: "ARAM_5x5", 70: "ONEFORALL_5x5", 72: "FIRSTBLOOD_1x1", 73: "FIRSTBLOOD_2x2", 75: "SR_6x6",
+	440: "RANKED_FLEX_SR", 470: "RANKED_FLEX_TT", 41: "RANKED_TEAM_3x3",
+	16: "ODIN_5x5_BLIND", 17: "ODIN_5x5_DRAFT", 25: "BOT_ODIN_5x5", 830: "BOT_5x5_INTRO",
+	840: "BOT_5x5_BEGINNER", 850: "BOT_5x5_INTERMEDIATE", 800: "BOT_TT_3x3",
+	61: "GROUP_FINDER_5x5", 450: "ARAM_5x5", 70: "ONEFORALL_5x5", 72: "FIRSTBLOOD_1x1", 73: "FIRSTBLOOD_2x2", 75: "SR_6x6",
 	76: "URF_5x5", 78: "ONEFORALL_MIRRORMODE_5x5", 83: "BOT_URF_5x5",
 	91: "NIGHTMARE_BOT_5x5_RANK1", 92: "NIGHTMARE_BOT_5x5_RANK2", 93: "NIGHTMARE_BOT_5x5_RANK5",
 	96: "ASCENSION_5x5", 98: "HEXAKILL", 100: "BILGEWATER_ARAM_5x5", 300: "KING_PORO_5x5",
-	310: "COUNTER_PICK", 313: "BILGEWATER_5x5", 315: "SIEGE", 317: "DEFINITELY_NOT_DOMINION_5x5",
+	310: "COUNTER_PICK", 313: "BILGEWATER_5x5", 940: "SIEGE", 317: "DEFINITELY_NOT_DOMINION_5x5",
 	318: "ARURF_5X5", 325: "ARSR_5x5", 400: "TEAM_BUILDER_DRAFT_UNRANKED_5x5",
 	430: "TB_BLIND_SUMMONERS_RIFT_5x5", 600: "ASSASSINATE_5x5", 610: "DARKSTAR_3x3"
 }
@@ -159,7 +159,7 @@ class Riot:
 	async def get_recent_matches_by_account(self, accountId, region):
 		try:
 			region = REGIONS_TO_ENDPOINTS_MAP[region.lower()]
-			return await self.call_riot('https://%s.api.riotgames.com/lol/match/v3/matchlists/by-account/%s/recent?api_key=%s' % (region, accountId, self.key))
+			return await self.call_riot('https://%s.api.riotgames.com/lol/match/v3/matchlists/by-account/%s?api_key=%s' % (region, accountId, self.key))
 		except Exception as ex:
 			raise
 
